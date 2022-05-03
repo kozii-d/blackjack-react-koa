1facca7b1850f310a0a1ba7d66e0b047c4b21846
Dmitrii Kozii <dkozii1807@gmail.com>
2022-05-03 15:12:32 +0300

    refactored loginPage

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -1,41 +1,53 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 
 const LoginPage = ({getToken}) => {
 
     const [formValue, setFormValue] = useState(['', '']);
 
-    const handleAddInput = () => {
+    const handleAddInput = useCallback(() => {
         setFormValue([...formValue, '']);
-    }
+    }, [formValue]);
 
-    const handleDeleteInput = (e) => {
-        setFormValue(formValue.filter((input, index) => index !== +e.target.parentNode.id));
-    }
+    const handleDeleteInput = useCallback((indexItem) => {
+        setFormValue(formValue.filter((input, i) => i !== indexItem));
+    }, [formValue]);
 
-    const handleSubmit = (e) => {
+    const handleSubmit = useCallback((e) => {
         e.preventDefault();
         getToken(formValue);
-    }
+    }, [formValue]);
 
-    const handleChange = (e) => {
-        setFormValue(formValue.map((item, index) => +e.target.parentNode.id === index ? e.target.value : item));
-    }
+    const handleChange = useCallback((e, indexItem) => {
+        setFormValue(formValue.map((item, index) => indexItem === index ? e.target.value : item));
+    }, [formValue]);
 
     return (
         <div className='login'>
             <h2 className='login__title'>create players</h2>
             <form className='login__form' onSubmit={handleSubmit}>
                 {formValue.map((item, index) => (
-                    <div className='login__item' key={index} id={index}>
-                        <input className='login__input' required type='text' name={`name${index}`} value={formValue[index]} onChange={handleChange} />
+                    <div className='login__item'
+                         key={index}
+                         id={index.toString()}
+                    >
+                        <input className='login__input'
+                               required
+                               type='text'
+                               name={`name${index}`}
+                               value={item}
+                               onChange={(e) => handleChange(e, index)}
+                        />
                         {
-                            formValue.length <= 2
-                            ? null
-                            : <button className='login__btn-close' type='button' onClick={handleDeleteInput}>✖</button>
+                            formValue.length > 2
+                            && <button className='login__btn-close'
+                                       type='button'
+                                       onClick={() => handleDeleteInput(index)}>✖</button>
                         }
                     </div>
                 ))}
-                <button className='login__btn-form login__btn-form_close' type='button' onClick={handleAddInput}>add player</button>
+                <button className='login__btn-form login__btn-form_close' type='button' onClick={handleAddInput}>add
+                    player
+                </button>
                 <button className='login__btn-form login__btn-form_send' type='submit'>start game</button>
             </form>
         </div>
